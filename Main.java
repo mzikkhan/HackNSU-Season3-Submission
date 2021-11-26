bfdd42d7642b19281d513a31235662f9d23f419b
abs211 <95068155+abs211@users.noreply.github.com>
2021-11-26 13:50:04 +0600

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,7 +1,6 @@
 package application;
 	
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
@@ -11,10 +10,11 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			ViewFactory showloginorsignup =new ViewFactory();
-			showloginorsignup.showView("Task1.fxml");
-			showloginorsignup.fooClass(LoginOrSignupController.class);
-			
+			BorderPane root = new BorderPane();
+			Scene scene = new Scene(root,400,400);
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			primaryStage.setScene(scene);
+			primaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
